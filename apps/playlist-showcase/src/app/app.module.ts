d77dbb4a119d61f6f3cc94e0427ae50647658b3a
Florin Borceanu <florin.borceanu@bytex.ro>
2022-04-24 13:50:44 +0300

    Architecture: NgRx added

--- apps/playlist-showcase/src/app/app.module.ts
+++ apps/playlist-showcase/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { ApplicationTitleService, ThemeService } from '@utils/services';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { themeFactory, translateHttpLoaderFactory } from '@utils/factories';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -14,6 +19,21 @@ import { BrowserModule } from '@angular/platform-browser';
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(routerReducer, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+      },
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+      autoPause: true,
+    }),
+    StoreRouterConnectingModule.forRoot(),
     TranslateModule.forRoot({
       defaultLanguage: 'en_US',
       loader: {
